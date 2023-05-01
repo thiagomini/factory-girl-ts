204b7cc25ba0d58fc7820487baccee1d77e4d323
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-01 10:59:08 -0300

    fix: fix association type

--- src/association.ts
+++ src/association.ts
@@ -2,10 +2,15 @@ import { ModelAdapter } from '@src/adapters/adapter.interface';
 import { Dictionary } from '@src/types';
 import { ValueOf } from 'type-fest';
 import { Builder } from './interfaces';
-export class Association<T, P = Dictionary, ReturnType = T> {
+export class Association<
+  Attributes,
+  Params = Dictionary,
+  ReturnType = Attributes,
+> {
   constructor(
-    private readonly builder: Builder<T, P, ReturnType>,
-    private readonly adapter: ModelAdapter<ReturnType>,
+    private readonly builder: Builder<Attributes, Params, ReturnType>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private readonly adapter: ModelAdapter<any, ReturnType>,
     private readonly key?: keyof ReturnType,
   ) {}
 
