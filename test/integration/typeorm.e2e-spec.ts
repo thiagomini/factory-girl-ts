85a29a63adcaf2cc2c364d247d0cacfce2f3dffb
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-03 19:01:59 -0300

    test(typeorm): build entity with relationship

--- test/integration/typeorm.e2e-spec.ts
+++ test/integration/typeorm.e2e-spec.ts
@@ -1,5 +1,12 @@
 import { FactoryGirl } from '@src/factory-girl';
-import { Column, DataSource, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  DataSource,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 class User {
@@ -15,6 +22,36 @@ class User {
     type: 'varchar',
   })
   email!: string;
+}
+
+@Entity()
+class Address {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({
+    type: 'varchar',
+  })
+  street!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  city!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  state!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  zip!: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn()
+  user!: User;
 }
 
 const dataSource = new DataSource({
@@ -49,5 +86,40 @@ describe('Typeorm integration', () => {
     expect(user.id).toBe(1);
     expect(user.name).toBe('John');
     expect(user.email).toBe('some-email@mail.com');
+  });
+
+  it('builds a model with association', () => {
+    // Arrange
+    const defaultAttributesFactory = () => ({
+      id: 1,
+      name: 'John',
+      email: 'some-email@mail.com',
+    });
+    const userFactory = FactoryGirl.define(User, defaultAttributesFactory);
+    const addressFactory = FactoryGirl.define(Address, () => ({
+      id: 1,
+      street: '123 Fake St.',
+      city: 'Springfield',
+      state: 'IL',
+      zip: '90210',
+      user: userFactory.associate(),
+    }));
+
+    // Act
+    const address = addressFactory.build();
+
+    // Assert
+    expect(address).toEqual<Address>({
+      city: 'Springfield',
+      id: 1,
+      state: 'IL',
+      street: '123 Fake St.',
+      zip: '90210',
+      user: {
+        id: 1,
+        name: 'John',
+        email: 'some-email@mail.com',
+      },
+    });
   });
 });
