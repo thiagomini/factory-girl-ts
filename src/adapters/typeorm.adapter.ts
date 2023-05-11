ebdddcbd01e8291207d568499a6b4372959fd9c8
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-11 18:56:38 -0300

    refactor(typeorm): move private method below

--- src/adapters/typeorm.adapter.ts
+++ src/adapters/typeorm.adapter.ts
@@ -10,9 +10,6 @@ export class TypeOrmRepositoryAdapter<
 > implements ModelAdapter<TClass, ReturnType>
 {
   constructor(private readonly dataSource: DataSource) {}
-  private getRepositoryForModel(model: TClass) {
-    return this.dataSource.getRepository(model);
-  }
 
   build(
     ModelClass: TClass,
@@ -28,7 +25,12 @@ export class TypeOrmRepositoryAdapter<
     const repository = this.getRepositoryForModel(modelClass);
     return await repository.save(model);
   }
+
   get<K extends keyof ReturnType>(model: ReturnType, key: K): ReturnType[K] {
     return model[key];
+  }
+
+  private getRepositoryForModel(model: TClass) {
+    return this.dataSource.getRepository(model);
   }
 }
