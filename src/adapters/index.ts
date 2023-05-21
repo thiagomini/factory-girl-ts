fa9f1d8bb40ef39640bd38dbaddcad32ddd3520e
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-21 10:21:33 -0300

    fix: export all required files

---
+++ src/adapters/index.ts
@@ -0,0 +1,4 @@
+export * from './adapter.interface';
+export * from './object.adapter';
+export * from './sequelize.adapter';
+export * from './typeorm.adapter';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from './interfaces';
 export * from './association';
+export * from './factory-girl';
+export * from './factory';
+export * from './utils';
+export * from './adapters';
+export * from './types';
