71a627d2494aa6ffa9267c44172069bba5ab6a04
Thiago Martins <thiago_valentim1@hotmail.com>
2023-08-26 09:17:44 -0300

    docs(factory): add docs for each method

--- src/factory-girl.ts
+++ src/factory-girl.ts
@@ -11,6 +11,23 @@ export class FactoryGirl {
 
   static sequences = new Map<string, number>();
 
+  /**
+   * Define a sequence of numbers to be used in the factory. The sequence is defined by a name and a callback
+   * that receives the current value of the sequence and returns the next value.
+   * @param id The name of the sequence.
+   * @param callback A function that receives the current value of the sequence and returns the next value.
+   * @returns The next value of the sequence.
+   * @example
+   * const userFactory = FactoryGirl.define<User>('User', () => ({
+   *    name: 'John Doe',
+   *    email: FactoryGirl.sequence('user.email', (n: number) => `test-${n}@mail.com`),
+   *  })
+   * )
+   * const user1 = await userFactory.build();
+   * const user2 = await userFactory.build();
+   * // user1.email === 'test-1@mail.com'
+   * // user2.email === 'test-2@mail.com'
+   */
   static sequence<T>(id: string, callback: (seq: number) => T) {
     let seq = FactoryGirl.sequences.get(id);
     if (seq === undefined) seq = 0;
@@ -23,6 +40,13 @@ export class FactoryGirl {
     this.adapter = adapter;
   }
 
+  /**
+   * Define a factory for a model, specifying its default attributes and options. The adapter should be defined
+   * to work properly.
+   * @param model The model to define a factory for. It can be either a model class or an interface.
+   * @param defaultAttributesFactory A function that returns the default attributes for the model.
+   * @returns A factory for the model.
+   */
   static define<
     ModelOrInterface,
     Attributes = DeepPartialAttributes<InstanceOrInterface<ModelOrInterface>>,
--- src/factory.ts
+++ src/factory.ts
@@ -32,6 +32,13 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     return this._adapter();
   }
 
+  /**
+   * Creates an association with the current factory. The model creation is postponed until
+   * necessary - if you call `build` or `create` on the factory but override the association,
+   * the association will not be created in the database.
+   * @param key The key of this model to be returned when the association is resolved.
+   * @returns An association object that can be used to build or create the associated model.
+   */
   associate<K extends keyof ReturnType>(
     k?: K,
   ): Association<Model, Attributes, Params, ReturnType>;
@@ -42,6 +49,7 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     K extends keyof ReturnType,
     O extends Override<Attributes, ReturnType>,
   >(key: K, override: O): Association<any, any, any, any>;
+
   associate<
     K extends keyof ReturnType,
     O extends Override<Attributes, ReturnType>,
@@ -59,6 +67,19 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Creates a model and saves it to the database. This is based on the default attributes and
+   * can be overridden by passing in an object.
+   * @param override An object that overrides the default attributes.
+   * @param additionalParams Additional params that can be used in the default attributes factory.
+   * @returns The created model.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   *
+   * const user = await userFactory.create({ name: 'Jane' });
+   *
+   * // user.name === 'Jane'
+   */
   async create(
     override?: Override<Attributes, ReturnType>,
     additionalParams?: Params,
@@ -78,6 +99,19 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     return await this.resolveCreateHooks(createdModel);
   }
 
+  /**
+   * Creates multiple models and saves them to the database
+   * @param count Number of models to create
+   * @param partials An array of partials that override the default attributes. Each partial will be used for one model. Alternatively,
+   * if you provide an object, it will be used for all models.
+   * @param additionalParams Additional params that can be used in the default attributes factory.
+   * @returns An array of the created models.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const users = await userFactory.createMany(2, [{ name: 'JohnChanged' }, { age: 21 }]);
+   * // users[0] === { name: 'JohnChanged', age: 20 }
+   * // users[1] === { name: 'John', age: 21 }
+   */
   async createMany(
     count: number,
     partials?:
@@ -95,6 +129,16 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Builds a model without saving it to the database.
+   * @param override An object that overrides the default attributes.
+   * @param additionalParams Additional params that can be used in the default attributes factory.
+   * @returns The built model.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const user = await userFactory.build({ name: 'Jane' });
+   * // user.name === 'Jane'
+   */
   async build(
     override?: Override<Attributes, ReturnType>,
     additionalParams?: Params,
@@ -116,6 +160,18 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     return await this.resolveBuildHooks(finalResult);
   }
 
+  /**
+   * Builds multiple models without saving them to the database.
+   * @param count Number of models to build
+   * @param partials An array of partials that override the default attributes. Each partial will be used for one model.
+   * @param additionalParams Additional params that can be used in the default attributes factory.
+   * @returns An array of the built models.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const users = await userFactory.buildMany(2, [{ name: 'JohnChanged' }, { age: 21 }]);
+   * // users[0] === { name: 'JohnChanged', age: 20 }
+   * // users[1] === { name: 'John', age: 21 }
+   */
   async buildMany(
     count: number,
     partials?:
@@ -135,6 +191,16 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Extends the current factory with additional default attributes.
+   * @param newDefaultAttributesFactory A function that returns the new default attributes.
+   * @returns A new factory with the extended default attributes.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const extendedUserFactory = userFactory.extend(() => ({ name: 'Jane' }));
+   * const user = await extendedUserFactory.build();
+   * // user.name === 'Jane'
+   */
   extend<ExtendedParams extends Params = Params>(
     newDefaultAttributesFactory: DefaultAttributesFactory<
       Attributes,
@@ -159,6 +225,19 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Adds a hook that is called after the model is created.
+   * @param afterCreateHook A function that is called after the model is created.
+   * @returns A new factory with the added hook.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const extendedUserFactory = userFactory.afterCreate((user) => {
+   *  user.name = 'Jane';
+   *  return user;
+   * });
+   * const user = await extendedUserFactory.create();
+   * // user.name === 'Jane'
+   */
   afterCreate(
     afterCreateHook: AfterCreateHook<ReturnType>,
   ): Factory<Model, Attributes, Params, ReturnType> {
@@ -170,6 +249,19 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Adds a hook that is called after the model is built.
+   * @param afterBuildHook A function that is called after the model is built.
+   * @returns A new factory with the added hook.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const extendedUserFactory = userFactory.afterBuild((user) => {
+   *   user.name = 'Jane';
+   *   return user;
+   * });
+   * const user = await extendedUserFactory.build();
+   * // user.name === 'Jane'
+   */
   afterBuild(
     afterBuildHook: AfterBuildHook<ReturnType>,
   ): Factory<Model, Attributes, Params, ReturnType> {
@@ -182,6 +274,18 @@ export class Factory<Model, Attributes, Params = any, ReturnType = Model> {
     );
   }
 
+  /**
+   * Mutates the model after it is created. This is useful if you want to change the model class or type.
+   * @param callback A function that is called after the model is created.
+   * @returns A new factory with the added hook.
+   * @example
+   * const userFactory = new Factory(User, () => ({ name: 'John', age: 20 }));
+   * const employeeFactory = userFactory.mutate((user) => new Employee(user));
+   *
+   * const employee = await employeeFactory.create();
+   *
+   * // employee instanceof Employee === true
+   */
   mutate<NewType>(
     callback: (model: InstanceOrInterface<Model>) => NewType | Promise<NewType>,
   ) {
