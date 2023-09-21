b77c881f5e6a84f78d8afff975f0d2313fb3e5dc
Thiago Martins <thiago_valentim1@hotmail.com>
2023-09-21 14:42:59 -0300

    feat(mikro-orm): add mikro orm fork option

--- src/adapters/mikro-orm.adapter.ts
+++ src/adapters/mikro-orm.adapter.ts
@@ -1,18 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { MikroORM } from '@mikro-orm/core';
+import { EntityManager, MikroORM } from '@mikro-orm/core';
 import { ModelAdapter } from './adapter.interface';
+
+export type MikroOrmAdapterOptions = {
+  shouldFork: boolean;
+};
 
 export class MikroOrmAdapter<TModelSchema, ReturnType>
   implements ModelAdapter<TModelSchema, ReturnType>
 {
-  constructor(private readonly orm: MikroORM) {}
+  constructor(
+    private readonly orm: MikroORM,
+    private readonly options: MikroOrmAdapterOptions = {
+      shouldFork: true,
+    },
+  ) {}
+
+  private shouldFork(): boolean {
+    return Boolean(this.options.shouldFork);
+  }
+
+  private geEntityManager(): EntityManager {
+    return this.shouldFork() ? this.orm.em.fork() : this.orm.em;
+  }
 
   build(ModelClass: TModelSchema, props: any) {
-    const modelRepository = this.orm.em.fork().getRepository(ModelClass as any);
+    const em = this.geEntityManager();
+    const modelRepository = em.getRepository(ModelClass as any);
     return modelRepository.create(props) as ReturnType;
   }
   async save(model: any): Promise<any> {
-    await this.orm.em.fork().persistAndFlush(model);
+    const em = this.geEntityManager();
+    await em.persistAndFlush(model);
     return model;
   }
   get<K extends string | number | symbol>(model: any, key: K) {
